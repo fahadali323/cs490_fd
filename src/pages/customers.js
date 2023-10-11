0e054e9783764dab141c8c9dbdb53dc35cc64dbc
fahadali323 <afahad630@gmail.com>
2023-10-11 16:09:05 -0400

    finished adding edit button

--- src/pages/customers.js
+++ src/pages/customers.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../styling/moviessearch.css';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Customers() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -11,6 +13,9 @@ function Customers() {
   const [resultsPerPage, setResultsPerPage] = useState(10); // Number of results to display per page
   const [availableResultsPerPage] = useState([10, 20, 30]); // Available options for results per page
   const [totalPages, setTotalPages] = useState(1);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingCustomer, setEditingCustomer] = useState(null);
+
 
   // eslint-disable-next-line 
   const fetchCustomers = async () => {
@@ -45,6 +50,39 @@ function Customers() {
     const timePart = date.toLocaleTimeString();
     return { datePart, timePart };
   };
+
+  const editCustomerDetails = (customerId) => {
+    // Find the customer to edit based on the customerId
+    const customerToEdit = customers.find((customer) => customer.customer_id === customerId);
+
+    setIsEditing(true);
+    setEditingCustomer(customerToEdit);
+  };
+  const handleEditCustomer = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/customers/update', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editingCustomer),
+      });
+
+      if (response.ok) {
+        // Customer updated successfully, refresh the customer list
+        toast.success('Customer added successfully');
+        // fetchCustomers();
+        setIsEditing(false); // Close the edit form
+      } else {
+        // Handle error here
+        console.error('Error updating customer');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
 
   const handleSearch = async () => {
     try {
@@ -99,6 +137,7 @@ function Customers() {
 
   return (
     <div>
+      <ToastContainer />
       <h1>Customer Search</h1>
       <div>
         <input
@@ -128,6 +167,7 @@ function Customers() {
               <th>Email</th>
               <th>Create Date</th>
               <th>Time</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -141,55 +181,110 @@ function Customers() {
                   <td>{customer.email}</td>
                   <td>{datePart}</td> {/* Display the date part */}
                   <td>{timePart}</td> {/* Display the time part */}
+                  <td>
+                    {/* //<button onClick={() => viewCustomerDetails(customer.customer_id)}>View</button> */}
+                    <button onClick={() => editCustomerDetails(customer.customer_id)}>Edit</button>
+                    {/* <button onClick={() => deleteCustomer(customer.customer_id)}>Delete</button> */}
+                  </td>
                 </tr>
               );
             })}
           </tbody>
         </table>
         {customers.length > resultsPerPage && (
-  <div className="pagination">
-    <button
-      onClick={() => handlePageChange(1)} // Go to the first page
-      disabled={currentPage === 1}
-    >
-      First
-    </button>
-    <button
-      onClick={() => handlePageChange(currentPage - 1)}
-      disabled={currentPage === 1}
-    >
-      Previous
-    </button>
-    <span>Page {currentPage} of {totalPages}</span>
-    <button
-      onClick={() => handlePageChange(currentPage + 1)}
-      disabled={currentPage === totalPages}
-    >
-      Next
-    </button>
-    <button
-      onClick={() => handlePageChange(totalPages)} // Go to the last page
-      disabled={currentPage === totalPages}
-    >
-      Last
-    </button>
-    <label>
-      Results per Page:
-      <select
-        value={resultsPerPage}
-        onChange={(e) => setResultsPerPage(parseInt(e.target.value, 10))}
-      >
-        {availableResultsPerPage.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </label>
-  </div>
-)}
+          <div className="pagination">
+            <button
+              onClick={() => handlePageChange(1)} // Go to the first page
+              disabled={currentPage === 1}
+            >
+              First
+            </button>
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </button>
+            <span>Page {currentPage} of {totalPages}</span>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+            <button
+              onClick={() => handlePageChange(totalPages)} // Go to the last page
+              disabled={currentPage === totalPages}
+            >
+              Last
+            </button>
+            <label>
+              Results per Page:
+              <select
+                value={resultsPerPage}
+                onChange={(e) => setResultsPerPage(parseInt(e.target.value, 10))}
+              >
+                {availableResultsPerPage.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+        )}
 
       </div>
+      {isEditing && editingCustomer && (
+        <div>
+          <h2>Edit Customer</h2>
+          <form onSubmit={handleEditCustomer}>
+            {/* Display customer details */}
+            <div>
+              <label>Customer ID: {editingCustomer.customer_id}</label>
+            </div>
+            <div>
+              <label>Store ID:</label>
+              <input
+                type="text"
+                value={editingCustomer.store_id}
+                onChange={(e) => setEditingCustomer({ ...editingCustomer, store_id: e.target.value })}
+              />
+            </div>
+            <div>
+              <label>First Name:</label>
+              <input
+                type="text"
+                value={editingCustomer.first_name}
+                onChange={(e) => setEditingCustomer({ ...editingCustomer, first_name: e.target.value })}
+              />
+            </div>
+            <div>
+              <label>Last Name:</label>
+              <input
+                type="text"
+                value={editingCustomer.last_name}
+                onChange={(e) => setEditingCustomer({ ...editingCustomer, last_name: e.target.value })}
+              />
+            </div>
+            <div>
+              <label>Email:</label>
+              <input
+                type="text"
+                value={editingCustomer.email}
+                onChange={(e) => setEditingCustomer({ ...editingCustomer, email: e.target.value })}
+              />
+            </div>
+            {/* ... Add input fields for other customer details ... */}
+            <div>
+              <button type="submit">Update</button>
+              <button onClick={() => setIsEditing(false)}>Cancel</button>
+            </div>
+          </form>
+        </div>
+      )}
+
+
     </div>
   );
 }
